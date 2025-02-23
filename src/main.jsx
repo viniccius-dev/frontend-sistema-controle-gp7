69c3ff213258f48b092b7d3e59aec3bf44f3dc53
Vinícius <santsmarcos81@gmail.com>
2025-02-23 19:37:17 -0300

    Instalando e configurando ui material

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { AppRoutes } from "./routes";
+import theme from "./styles/theme";
+import GlobalStyles from "./styles/global";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AppRoutes />
+    </ThemeProvider>
+  </React.StrictMode>
 )
---
+++ src/routes/index.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route, Navigate } from  "react-router-dom";
+
+import { SignIn } from "../pages/SignIn";
+
+export function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<SignIn />} />
+
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
